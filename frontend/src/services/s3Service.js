cbf8b06a0f4e1cb6faf1d94434717c33db9ad17b
Pururaj Choudhary <95862609+Pururaj069@users.noreply.github.com>
2025-06-11 14:08:14 +0530

    Update s3Service.js

--- frontend/src/services/s3Service.js
+++ frontend/src/services/s3Service.js
@@ -1,6 +1,6 @@
 // src/services/s3Service.js
 export const fetchModelsFromBackend = async () => {
-  const response = await fetch("http://localhost:4000/api/models");
+  const response = await fetch("/api/models");               // const response = await fetch("http://localhost:4000/api/models"); --> use this if not using reverse proxy 
   if (!response.ok) throw new Error("Failed to fetch models");
   return response.json();
 };
